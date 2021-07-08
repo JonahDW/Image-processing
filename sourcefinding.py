8c3d56ac6d3824ac6076252b8a75bc4fa42e3900
Jonah Wagenveld <wagenveld@pc002.mpifr-bonn.mpg.de>
2021-07-08 15:07:44 +0200

    Spectral index rework

--- sourcefinding.py
+++ sourcefinding.py
@@ -16,12 +16,15 @@ from pathlib import Path
 from astropy.wcs import WCS
 from astropy.io import fits
 from astropy import units as u
+from astropy.stats import sigma_clip
 from astropy.table import Table, Column
 from astropy.coordinates import SkyCoord
+from scipy import ndimage
 
 from regions import EllipseSkyRegion, write_crtf, write_ds9
 
 import bdsf
+import casacore.images as pim
 
 def run_bdsf(image, argfile):
     '''
@@ -54,23 +57,43 @@ def run_bdsf(image, argfile):
 
     return outcatalog
 
-def read_alpha(imname, catalog, regions):
+def read_alpha(inpimage, catalog, regions):
     '''
     Determine spectral indices of the sources
     '''
-    path = Path(__file__).parent
+    imname = os.path.join(os.path.dirname(inpimage),
+                          os.path.basename(inpimage).split('.')[0])
+    tt0 = pim.image(imname+'.image.tt0')
+    tt0.putmask(False)
+    tt0.tofits(imname+'_tt0.fits')
+    tt0 = fits.open(imname+'_tt0.fits')
 
-    os.system(f'casa --nologfile -c {os.path.join(path,"smooth_alpha.py")} {imname}')
-    dirname = os.path.dirname(imname)
+    tt1 = pim.image(imname+'.image.tt1')
+    tt1.putmask(False)
+    tt1.tofits(imname+'_tt1.fits')
+    tt1 = fits.open(imname+'_tt1.fits')
 
-    alpha = fits.open(os.path.join(dirname,'smooth_alpha.fits'))
-    alpha_err = fits.open(os.path.join(dirname,'smooth_alpha_error.fits'))
-
-    wcs = WCS(alpha[0].header)
+    # Get WCS from header and drop freq and stoke axes
+    wcs = WCS(tt0[0].header)
+    wcs = wcs.dropaxis(3)
+    wcs = wcs.dropaxis(2)
 
     pixel_regions = [region.to_pixel(wcs) for region in regions]
-    image = alpha[0].data[0,0,:,:]
-    err_image = alpha_err[0].data[0,0,:,:]
+
+    alpha = tt1[0].data[0,0,:,:]/tt0[0].data[0,0,:,:]
+    alpha = sigma_clip(alpha, sigma=3, masked=True)
+
+    # Smooth image with NaNs
+    U = alpha.filled(np.nan)
+    V = U.copy()
+    V[np.isnan(U)]=0
+    VV = ndimage.gaussian_filter(V, sigma=5, order=0)
+
+    W = 0*U.copy()+1
+    W[np.isnan(U)]=0
+    WW = ndimage.gaussian_filter(V, sigma=5, order=0)
+
+    alpha == VV/WW
 
     alpha_list = []
     alpha_err_list = []
@@ -78,42 +101,35 @@ def read_alpha(imname, catalog, regions):
         pixel_region = pixel_regions[i]
 
         mask = pixel_region.to_mask(mode='center')
-        mask_data = mask.to_image(image.shape).astype(bool)
+        mask_data = mask.to_image(alpha.shape).astype(bool)
 
-        alpha_values = image[mask_data]
-        alpha_err_values = err_image[mask_data]
+        alpha_values = alpha[mask_data]
+        weights = tt0[0].data[0,0,:,:][mask_data]
 
+        weights = weights[~np.isnan(alpha_values)]
         alpha_values = alpha_values[~np.isnan(alpha_values)]
-        alpha_err_values = alpha_err_values[~np.isnan(alpha_err_values)]
-
-        alpha_values = alpha_values[alpha_err_values < 1.0]
-        alpha_err_values = alpha_err_values[alpha_err_values < 1.0]
 
         if len (alpha_values) > 0:
-            alpha_tot = np.sum(alpha_values/alpha_err_values)/np.sum(1/alpha_err_values)
-            alpha_err_tot = np.mean(alpha_err_values)
-
-            alpha_list.append(alpha_tot)
-            alpha_err_list.append(alpha_err_tot)
+            alpha_mean = np.sum(alpha_values*weights)/np.sum(weights)
+            alpha_std = np.sqrt(np.sum(weights*(alpha_values-alpha_mean)**2) / 
+                               (np.sum(weights)*(len(weights)-1 / len(weights))))
+            alpha_list.append(alpha_mean)
+            alpha_err_list.append(alpha_std)
         else:
             alpha_list.append(np.ma.masked)
             alpha_err_list.append(np.ma.masked)
 
-    catalog['alpha'] = alpha_list
-    catalog['alpha_err'] = alpha_err_list
+    a = Column(alpha_list, name='Spectral_index')
+    b = Column(alpha_err_list, name='E_Spectral_index')
+    catalog.add_columns([a,b], indexes=[10,10]) 
 
     # Clean up
-    os.remove(os.path.join(dirname,'smooth_alpha.fits'))
-    os.remove(os.path.join(dirname,'smooth_alpha_error.fits'))
-
-    files = os.listdir('.')
-    for f in files:
-        if f.endswith('.last') or f.endswith('false'):
-            os.remove(f)
+    os.remove(imname+'_tt0.fits')
+    os.remove(imname+'_tt1.fits')
 
     return catalog
 
-def transform_cat(catalog):
+def transform_cat(catalog, survey_name):
     '''
     Add names for sources in the catalog following IAU naming conventions
     '''
@@ -127,7 +143,7 @@ def transform_cat(catalog):
                             [source['DEC'] for source in catalog],
                             unit=(u.deg,u.deg))
 
-    ids = ['MALS J{0}{1}'.format(coord.ra.to_string(unit=u.hourangle,
+    ids = [survey_name+' J{0}{1}'.format(coord.ra.to_string(unit=u.hourangle,
                                                      sep='',
                                                      precision=0,
                                                      pad=True),
@@ -144,7 +160,7 @@ def transform_cat(catalog):
 
     # Add columns at appropriate indices
     col_a = Column(pointing_name, 'Pointing_id')
-    col_b = Column(ids, name='MALS_id')
+    col_b = Column(ids, name=survey_name+'_id')
     col_c = Column(dra, name='dRA')
     col_d = Column(ddec, name='dDEC')
     catalog.add_columns([col_a, col_b, col_c, col_d],
@@ -221,6 +237,7 @@ def main():
     plot = args.plot
     ds9 = args.ds9
     spectral_index = args.spectral_index
+    survey = args.survey
 
     if mode in 'cataloging':
         bdsf_args = 'parsets/bdsf_args_cat.json'
@@ -243,7 +260,7 @@ def main():
         plot_sf_results(f'{imname}_ch0.fits', f'{imname}_rms.fits', bdsf_regions, plot)
 
     if spectral_index:
-        bdsf_cat = read_alpha(imname, bdsf_cat, bdsf_regions)
+        bdsf_cat = read_alpha(inpimage, bdsf_cat, bdsf_regions)
 
     if ds9:
         if ds9 is True:
@@ -257,7 +274,7 @@ def main():
     # Determine output by mode
     if mode in 'cataloging':
         outfile = imname+'_catalog.fits'
-        bdsf_cat = transform_cat(bdsf_cat)
+        bdsf_cat = transform_cat(bdsf_cat, survey)
         print(f'Wrote catalog to {outfile}')
         bdsf_cat.write(outfile, overwrite=True)
 
@@ -295,12 +312,12 @@ def new_argument_parser():
                                 provide an output filename (default = do
                                 not plot the results).""")
     parser.add_argument("--spectral_index", action='store_true',
-                        help="""Measure the spectral indices in the .alpha and
-                                alpha.error images. (assuming they are in the
-                                same directory as the input image) and include
-                                them in the catalog. This requires CASA
-                                functionalities (default = do not measure
+                        help="""Measure the spectral indices of the sources.
+                                this requires the presence of a tt0 and tt1
+                                image in the same folder (default = do not measure
                                 spectral indices).""")
+    parser.add_argument("--survey", default='MALS',
+                        help="Name of the survey to be used in source ids")
     return parser
 
 if __name__ == '__main__':
