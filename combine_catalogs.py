c24af958072166e7d4ecc9e41873946879738fdc
Jonah Wagenveld <wagenveld@pc002.mpifr-bonn.mpg.de>
2021-12-10 11:52:04 +0100

    Add source overlap, enhance combining catalogs

--- combine_catalogs.py
+++ combine_catalogs.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-from astropy.table import Table, vstack
+from astropy.table import Table, vstack, hstack, join
 
 from astropy.utils.metadata import enable_merge_strategies
 from astropy.utils.metadata import MergeStrategy
@@ -15,12 +15,35 @@ class MergeNumbersAsList(MergeStrategy):
         else:
             return None
 
-def combine_catalogs(catalogs, output_cat):
+def vstack_catalogs(catalogs, output_cat):
     cats = []
     for cat in catalogs:
         cats.append(Table.read(cat))
     with enable_merge_strategies(MergeNumbersAsList):
         full_cat = vstack(cats)
+
+    print('Writing full catalog to '+output_cat)
+    full_cat.write(output_cat, format='fits', overwrite=True)
+
+    return output_cat
+
+def hstack_catalogs(catalogs, output_cat):
+    cats = []
+    for cat in catalogs:
+        cats.append(Table.read(cat))
+    with enable_merge_strategies(MergeNumbersAsList):
+        full_cat = hstack(cats)
+
+    print('Writing full catalog to '+output_cat)
+    full_cat.write(output_cat, format='fits', overwrite=True)
+
+    return output_cat
+
+def join_catalogs(catalogs, output_cat, join_type, keys):
+    left = Table.read(catalogs[0])
+    right = Table.read(catalogs[1])
+
+    full_cat = join(left, right, keys=keys, join_type=join_type)
 
     print('Writing full catalog to '+output_cat)
     full_cat.write(output_cat, format='fits', overwrite=True)
@@ -33,8 +56,19 @@ def main():
 
     input_cats = args.input_cats
     output_cat = args.output_cat
+    mode = args.mode
+    keys = args.keys
 
-    combine_catalogs(input_cats, output_cat)
+    if mode == 'vstack':
+        vstack_catalogs(input_cats, output_cat)
+    if mode == 'hstack':
+        hstack_catalogs(input_cats, output_cat)
+    if 'join' in mode:
+        if len(mode.split(' ')) > 1:
+            join_type = mode.split(' ')[0]
+        else:
+            join_type = 'inner'
+        join_catalogs(input_cats, output_cat, join_type, keys)
 
 def new_argument_parser():
 
@@ -44,6 +78,12 @@ def new_argument_parser():
                         help="""Pointing catalogs made by PyBDSF, to be combined.""")
     parser.add_argument('output_cat',
                         help="""Name of the full output catalog""")
+    parser.add_argument('-m', '--mode', default='vstack',
+                        help="""How to combine tables. Supports vstack, hstack, or
+                                different join types (inner join, outer join, etc.)
+                                default = vstack""")
+    parser.add_argument('-k', '--keys', nargs='+', default='Source_id',
+                        help="""Key column(s) for join""")
 
     return parser
 
